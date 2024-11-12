25b7988b0bb46c79419a53111cfcf058db188032
wormgod <pgbc303@gmail.com>
2024-11-12 19:19:55 +0300

    Добавил запись о продажах

---
+++ Laboratory work 2/SalesRealization.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_work_2
+{
+    public struct SalesRealization
+    {
+        public int quantity;
+        public int day;
+        public string genre;
+    }
+
+    public void AddSale(string genree, int quantityy, int dayy)
+    {
+        SalesRealization sale = new SalesRealization { genre = genree, day = dayy, quantity = quantityy };
+    }
+}
